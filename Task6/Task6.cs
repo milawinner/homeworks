4f94b5d1281bc2485ce0240df788f860e52f6184
Mila <lakralima@mail.ru>
2022-08-31 02:56:53 +0300

    Task6 is ready

--- Task6/Task6.cs
+++ Task6/Task6.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Это целое число");
+}
+else
+{
+    Console.WriteLine("Это число не является целым");
+}
+
