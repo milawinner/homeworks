482f7faa41c16621c4271b19ade89f03e43a3219
Mila <lakralima@mail.ru>
2022-08-30 22:13:53 +0300

    Task4 in the new folder

---
+++ Task4/Task4.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num3 = Convert.ToInt32(Console.ReadLine());
