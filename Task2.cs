fb488d8daae37ca077fa43261ac954bc29693f7f
Mila <lakralima@mail.ru>
2022-08-30 21:36:15 +0300

    Task2 starting

--- Task2.cs
+++ Task2.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int num = ConvertToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num = ConvertToInt32(Console.ReadLine());
