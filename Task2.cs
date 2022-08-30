4f28e94dca16a73c3ebaf8cccce774bf831a4d70
Mila <lakralima@mail.ru>
2022-08-30 21:54:11 +0300

    Task2 full

--- Task2.cs
+++ Task2.cs
@@ -1,4 +1,14 @@
 ﻿Console.WriteLine("Введите первое число");
-int num = ConvertToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int num = ConvertToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.Write(num1);
+    Console.WriteLine(" - это большее число");
+}
+else
+{
+    Console.Write(num2);
+    Console.WriteLine(" - это большее число");
+}
