8f2c4aff8ca2a925b859918e4b2b23adf5c4e5f3
Mila <lakralima@mail.ru>
2022-08-31 02:36:05 +0300

    Task4 is ready

--- Task4/Task4.cs
+++ Task4/Task4.cs
@@ -2,5 +2,24 @@
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+Console.WriteLine("Введите третье число");
 int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2 & num1 >= num3)
+{
+    Console.Write(num1);
+    Console.WriteLine(" - это самое большее число");
+}
+else
+{
+    if (num2 > num1 & num2 > num3)
+{
+    Console.Write(num2);
+    Console.WriteLine(" - это самое большее число");
+}
+    else 
+    {
+        Console.Write(num3); 
+        Console.WriteLine(" - это самое большее число");
+        }
+}
